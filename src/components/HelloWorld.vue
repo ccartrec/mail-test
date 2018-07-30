c66d9baed6a90f1aaa6b3d6f7391a5509c1bd94d
Александр <cahei29@rambler.ru>
2018-07-30 23:48:14 +0300

    Дизайн

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,42 @@
 <template>
-  <div>
-    <div class="letters">
-      <div class="letters-item" v-for="(letter, idx) in outgoing" :key="idx">
-        <div class="date">{{ new Date(+letter.date.seconds * 1000) }}</div>
-        <div class="sender">{{ letter.sender }}</div>
-        <div class="subject">{{ letter.subject }}</div>
-        <div class="actions">
-          <button class="button is-small is-danger" @click="deleteLocation(location.id)">
-            Удалить
-          </button>
-        </div>
-      </div>
-      <!--div class="letters-item" v-for="(letter, idx) in incoming" :key="idx">
-        <div class="date">{{ new Date(+letter.date.seconds * 1000) }}</div>
-        <div class="sender">{{ letter.sender }}</div>
-        <div class="subject">{{ letter.subject }}</div>
-        <div class="actions">
-          <button class="button is-small is-danger" @click="deleteLocation(location.id)">
-            Удалить
-          </button>
-        </div>
-      </div-->
+  <div class="mailbox">
+    <div class="controls">
+      <div class="btn">Новое письмо</div>
+      <a>Входящие</a>
+      <a>Исходящие</a>
     </div>
-
-    <hr>
-
-    <form @submit="addLocation(subject, message)">
-      <h2>Новое сообщение</h2>
-      <input v-model="subject" placeholder="Тема" class="input">
-      <input v-model="message" placeholder="Сообщение" class="input">
-      <button type="submit" class="button is-success">Отправить</button>
-    </form>
+    <div class="letters-list">
+      <div class="letters">
+        <div class="letters-item" v-for="(letter, idx) in outgoing" :key="idx">
+          <div class="date">{{ new Date(+letter.date.seconds * 1000) }}</div>
+          <div class="sender">{{ letter.sender }}</div>
+          <div class="subject">{{ letter.subject }}</div>
+          <div class="actions">
+            <button class="button is-small is-danger" @click="deleteLocation(location.id)">
+              Удалить
+            </button>
+          </div>
+        </div>
+        <!--div class="letters-item" v-for="(letter, idx) in incoming" :key="idx">
+          <div class="date">{{ new Date(+letter.date.seconds * 1000) }}</div>
+          <div class="sender">{{ letter.sender }}</div>
+          <div class="subject">{{ letter.subject }}</div>
+          <div class="actions">
+            <button class="button is-small is-danger" @click="deleteLocation(location.id)">
+              Удалить
+            </button>
+          </div>
+        </div-->
+      </div>
+    </div>
+    <div class="messagebox">
+      <form @submit="addLocation(subject, message)">
+        <h2>Новое сообщение</h2>
+        <input v-model="subject" placeholder="Тема" class="input">
+        <input v-model="message" placeholder="Сообщение" class="input">
+        <button type="submit" class="button is-success">Отправить</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -69,28 +75,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  .letters-item {
-    display: flex;
-    border-bottom: 1px solid red;
-  }
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-input, button {
-  margin-bottom: 10px;
-}
-</style>
